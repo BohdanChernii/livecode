55862a644a61301788c4a5efc7074eb94ca4d515
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-08-18 16:11:35 +0300

    QWQQ

--- truewaytodotask/script.js
+++ truewaytodotask/script.js
@@ -1,4 +1,3 @@
-
 /* /aglo */
 /* 1. iterate from 2 to num
 
@@ -36,21 +35,20 @@ output:
 // }
 // getPrimes(15);
 
-function isPrime(number){
-    for(let index = 2;   index<number; index+=1){
-        
-        if(number%index === 0){
-            return false
-        }
+function isPrime(number) {
+for (let index = 2; index < number; index += 1) {
+    if (number % index === 0) {
+      return false;
     }
-        return true
-};
+  }
+  return true;
+}
 
-function getPrimes(num){
-    for(index = 2; index <= num; index += 1){
-        if(isPrime(index)){
-            console.log(index);
-        }
+function getPrimes(num) {
+  for (index = 2; index <= num; index += 1) {
+    if (isPrime(index)) {
+      console.log(index);
     }
+  }
 }
 getPrimes(15);
