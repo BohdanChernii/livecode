6969ab0a75dcabab72d01426b2bb32e056753d52
BohdanChernii <64136178+BohdanChernii@users.noreply.github.com>
2021-10-18 13:55:29 +0300

    wq

--- PagginationPage/src/Pagination.jsx
+++ PagginationPage/src/Pagination.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 const Pagination = ({pageNumber, handleNext, handlePrev, usersCount, usersPerPage}) => {
 const isPrevDisabled = (pageNumber > 1)
+const isNextDisable = usersCount / usersPerPage > pageNumber;
 return(
     <div className="pagination">
     <button className="btn" onClick={handlePrev} disabled={!isPrevDisabled}>
       {isPrevDisabled && '←'}
     </button>
     <span className="pagination__page">{pageNumber}</span>
-    <button className="btn" onClick={handleNext}>→</button>
+    <button className="btn" onClick={handleNext} disabled={!isNextDisable}>{isNextDisable && '→'}</button>
   </div>
 )
 
